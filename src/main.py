b4c3a96431cff93eb9a2c50a6107b1406a1f1fdc
jayce008 <jayce.ardon@gmail.com>
2025-01-23 09:16:23 +0100

    implemented basic setup

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+	text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+	print("hello world")
+	print(text_node)
+	
+main()
---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return self == other
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
