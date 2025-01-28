7d27f587c951592a8bfcc3434da8bc33cfdd1a4f
jayce008 <jayce.ardon@gmail.com>
2025-01-28 13:30:01 +0100

    extract images and links

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from htmlnode import LeafNode, ParentNode
-from markdown_parser import split_nodes_delimiter
+from markdown_parser import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
 from textnode import TextNode, TextType
 
 def main():
@@ -18,9 +18,11 @@ def main():
 
 	# print(node.to_html())
 
-	node = TextNode("This is text with 2 `code``blocks`", TextType.TEXT)
+	node = TextNode("This is text with 2 `code``blocks` word", TextType.TEXT)
 	new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-	print(node)
-	print(new_nodes)
-	
+
+	extract_markdown_images("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)")
+
+	text = "This is text with a link"
+	print(extract_markdown_links(text))
 main()
--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 
@@ -13,10 +14,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if len(segments) % 2 == 0:
             raise ValueError("Unmatched delimiters found in text.")
 
-        for i in len(segments):
+        for i in range(len(segments)):
             if i % 2 == 0:
                 new_node = TextNode(segments[i], TextType.TEXT)
             else:
                 new_node = TextNode(segments[i], text_type)
             node_lst.append(new_node)
     return node_lst
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+    return matches
